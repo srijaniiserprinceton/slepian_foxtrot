1ba469e76e460be5ff0a210c1354a3020b7f2837
Frederik J. Simons <fjsimons@gmail.com>
2020-02-19 13:51:11 -0500

    Plots Slepian functions for the disk in the spatial domain

--- swfried2d.m
+++ swfried2d.m
@@ -1,92 +1,118 @@
-function swfried2d
-% swfried2d
+function swfried2d(method)
+% swfried2d(method)
+%
+% FIGURE 3.4 of SIMONS & WANG
+%
+% INPUT:
+%
+% method   'SE' by Slepian "extension" [faster!]
+%          'GL' by direct Gauss-Legendre integration [slow!]
+%          'RS' by direct Riemann summation [slow!]
+%          'compare' by comparison of the methods
 %
 % Makes a fried-egg plot for +- angular orders for concentration to a
-% disk-shaped domain. The eigenvalues will be ever so slightly different
-% from the "truth" that we know so well - they may not occur exactly in
-% pairs, etc. But overall this is pretty well behaved to a part in a
-% million or so starting from reasonable choice.
+% disk-shaped domain. If 'method' is 'GL' The eigenvalues will be ever so
+% slightly different from the "truth" that we know so well - they may not
+% occur exactly in pairs, etc. But overall this is pretty well behaved to
+% a part in a million or so starting from reasonable choice.
 %
 % SEE ALSO:
 %
 % SWVALS2D
 %
-% Last modified by fjsimons-at-alum.mit.edu, 04/23/2009
+% Last modified by fjsimons-at-alum.mit.edu, 06/15/2010
 
 % eps2png -width 850 swfried2d.eps 
 % convert swfried2d.png swfried2d.gif
 
 % Somehow this performs best with a KEYBOARD before the print statement
 
-% Here we both simulataneously check and compute the eigenfunctions
+% Here we both simultaneously check and compute the eigenfunctions
+
+defval('method','SE');
 
 % Make a circle of some description
 circn=256;
 XY=[cos(linspace(0,2*pi,circn)) ; sin(linspace(0,2*pi,circn))]';
+
 % What is the mean distance between the points
 mdis=mean(sum(sqrt(diff(XY).^2),2));
+
 % Perform the localization
 N=42; % The Shannon number 
 J=30; % The number of Slepian functions that will be calculated
-method='GL';
+
 % Define the (resolution of the) output grid
 j=7;
 xp=linspace(-2,2,2^j);
 yp=linspace(-2,2,2^j);
 [XP,YP]=meshgrid(xp,yp);
 XYP=[XP(:) YP(:)];
-c11=[min(XYP(:,1)) max(XYP(:,2))];
-cmn=[max(XYP(:,1)) min(XYP(:,1))];
-% This returns all of the numerical eigenvalus
-[G,H,V,K,XYP,XY,A]=localization2D(XY,N,J,method,[],[],[],XYP);
-G=reshape(G,2^j,2^j,J);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Now we need to compare this to the other way of calculating it
-% The upper of number of orders should "cover" all the cases above
-M=8;
-% Calculate at least all of those that have eigenvalues of larger than 0.5
-[Nm,Nsum]=nsubm(N,M);
-% Form the spatial eigenfunction
-R=sqrt(XP.^2+YP.^2);
-TH=atan2(YP,XP);
-[Rx,iR,jR]=unique(R);
-clear E EV EM
-% Calculate the radial functions at the unique radial points that will be needed
-for m=0:M
-  [E{m+1},EV{m+1}]=swdisk(m,N,ceil(Nm(m+1)),[],Rx,'SE',0);
-  EM{m+1}=repmat(m,1,ceil(Nm(m+1)));
+% Now do the actual calculation
+switch method
+  case {'GL','RS'}
+   % This returns all of the numerical eigenvalus
+   [G,H,V,K,XYP,XY,A]=localization2D(XY,N,J,method,[],[],[],XYP);
+   G=reshape(G,2^j,2^j,J);
+ 
+   % Make the figure - with G/E or GV/EV for comparison
+   theG=G; 
+   theV=V;
+ case 'SE'
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Now we need to compare this to the other way of calculating it
+  % The upper of number of orders should "cover" all the cases above
+  M=8;
+  % Calculate at least all of those that have eigenvalues of larger than 0.5
+  [Nm,Nsum]=nsubm(N,M);
+  % Form the spatial eigenfunction
+  R=sqrt(XP.^2+YP.^2);
+  TH=atan2(YP,XP);
+  [Rx,iR,jR]=unique(R);
+  % Calculate the radial functions at the unique radial points that will be needed
+  for m=0:M
+    [E{m+1},EV{m+1}]=swdisk(m,N,ceil(Nm(m+1)),[],Rx,'SE',0);
+    EM{m+1}=repmat(m,1,ceil(Nm(m+1)));
+  end
+  keyboard
+  % Sort them all and compare eigenfunctions with the same eigenvalue
+  E=[E{:}]; EV=[EV{:}]; EM=[EM{:}];
+  [EV,i]=sort(EV,'descend'); E=E(:,i); EM=EM(i);
+  % Repeat the nonzero orders twice and take as many as you had
+  dbl=~~EM+1;
+  % The indexing sequence in the non-repeated vectors
+  seq=indeks(gamini(1:length(EV),dbl),1:J);
+  % This will change the sine into the cosine later
+  addph=~diff([0 seq]);
+  % Make the eigenvalue sequence with the repeats
+  EV=indeks(gamini(EV,dbl),1:J);
+  % Reassemble and combine with the sine/cosine functions
+  GV=nan(size(R,1),size(R,2),J);
+  for i=1:length(EV)
+    GV(:,:,i)=reshape(E(jR,seq(i)),size(R)).*...
+	      cos(EM(seq(i)).*TH-addph(i)*pi/2);
+  end
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  % Make the figure - with G/E or GV/EV for comparison
+  theG=GV;
+  theV=EV;
+ case 'compare'
+  keyboard 
+  % Not properly written but largely irrelevant now
+  % Check how close these eigenvalues are now
+  difer(EV-V,4)
 end
-% Sort them all and compare eigenfunctions with the same eigenvalue
-E=[E{:}]; EV=[EV{:}]; EM=[EM{:}];
-[EV,i]=sort(EV,'descend'); E=E(:,i); EM=EM(i);
-% Repeat the nonzero orders twice and take as many as you had
-dbl=~~EM+1;
-% The indexing sequence in the non-repeated vectors
-seq=indeks(gamini(1:length(EV),dbl),1:length(V));
-% This will change the sine into the cosine later
-addph=~diff([0 seq]);
-% Make the eigenvalue sequence with the repeats
-EV=indeks(gamini(EV,dbl),1:length(V));
-% Check how close these eigenvalues are now
-difer(EV-V,4)
-% Reassemble and combine with the sine/cosine functions
-GV=nan(size(G));
-for i=1:length(EV)
-  GV(:,:,i)=reshape(E(jR,seq(i)),size(R)).*...
-	    cos(EM(seq(i)).*TH-addph(i)*pi/2);
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Make the figure - with G/E or GV/EV for comparison
-theG=G; 
-% theG=GV;
-theV=V;
-% theV=EV;
+  
+keyboard
 
 % They should differ slightly - in the sign - in the phase
 clf
 [ah,ha,H]=krijetem(subnum(6,5));
+
+c11=[min(XYP(:,1)) max(XYP(:,2))];
+cmn=[max(XYP(:,1)) min(XYP(:,1))];
 
 for index=1:length(ah)
   axes(ah(index))
@@ -115,5 +141,4 @@ figdisp([],[],[],0)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function data=setnans(data)
 data(abs(data)<max(abs(data(:)))/100)=NaN;
-
 
